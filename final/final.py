6ec61a85bcdc1ec4c6b91e6fb977b6a5f412a55c
Ricky Mangerie <ricky@drexel.edu>
2020-08-20 01:00:11 -0400

    Calculated results for Semifinal Round, created script for Final Round

---
+++ final/final.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import sys
+import csv
+
+def countSeasons(s):
+
+    if s == "Season 5":
+        seasonsVotes[0] += 1
+    elif s == "All Stars 2":
+        seasonsVotes[1] += 1
+    else:
+        print("ERROR: Invalid entry ({})\n".format(s))
+
+
+def parseCSV():
+    totalVotes = 0
+
+    for row in seasons:
+        totalVotes += 1
+        print("\t{}".format(row[1]))
+        countSeasons(row[1])
+
+    return totalVotes
+
+def calcPercent():
+    i = 1
+    for n in seasonsVotes:
+        p = (n / totalVotes) * 100
+        pRound = round(p, 3)
+
+        if i == 1:
+           print("Season 5\t\t{}\t{}%".format(n, pRound))
+        elif i == 2:
+            print("All Stars 2\t\t{}\t{}%".format(n, pRound))
+        else:
+            print("ERROR: Case out of bounds ({})\n".format(i))
+
+        i += 1
+
+
+if __name__ == "__main__":
+    try:
+        csvFile = sys.argv[1];
+        with open(csvFile, newline='') as csvfile:
+            seasons = csv.reader(csvfile, delimiter=',')
+            next(seasons)
+
+            seasonsVotes = [0, 0]
+
+            print("All Entries:\n")
+            totalVotes = parseCSV()
+            print("\nTotal Votes\t{}\n".format(totalVotes))
+
+            calcPercent()
+    except IndexError:
+        print("ERROR: Must supply a CSV file")
+
+
+
+
